ba65dda159f286ef86f90b749b056305403b67f2
jsuare00 <jsuare00@uafs.edu>
2025-10-10 18:30:18 -0500

    Add home.php

---
+++ home.php
@@ -0,0 +1,10 @@
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <title>PHP Test</title>
+    </head>
+    <body>
+        <?php echo "<p>Hello from PHP!</p>"; ?>
+        <p>This is static HTML.</p>
+    </body>
+    </html>
